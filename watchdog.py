e4fa0e324b19253fdaf0a11fc606a396e1492a90
Dmytrii Lysak <dlysak@live.com>
2026-07-13 17:14:06 +0300

    Add `_parse_dt` utility to handle ISO datetime parsing in `watchdog.py`

--- watchdog.py
+++ watchdog.py
@@ -39,6 +39,15 @@ PROVIDER_ICONS = {
 }
 
 
+def _parse_dt(value: str | None) -> datetime | None:
+    if not value:
+        return None
+    try:
+        return datetime.fromisoformat(value.replace("Z", "+00:00"))
+    except (ValueError, AttributeError):
+        return None
+
+
 # ---------------------------------------------------------------------------
 # Data fetching
 # ---------------------------------------------------------------------------
